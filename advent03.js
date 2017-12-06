e308d8c7cbed059ffec329ab35036f86b1bfb78c
Douglas <dlowder@salesforce.com>
2017-12-06 15:48:05 -0800

    Efficient solution for day 3 part 2

--- advent03.js
+++ advent03.js
@@ -15,10 +15,65 @@ const solution1 = function(input) {
   }
 };
 
+const hash = function(loc) {
+  return '' + 1000 * loc[0] + loc[1] + 5000;
+};
+
+const sumOfAdjacentSquares = function(squares, loc) {
+  let locs = [
+    [loc[0] - 1, loc[1] - 1],
+    [loc[0], loc[1] - 1],
+    [loc[0] + 1, loc[1] - 1],
+    [loc[0] - 1, loc[1]],
+    [loc[0] + 1, loc[1]],
+    [loc[0] - 1, loc[1] + 1],
+    [loc[0], loc[1] + 1],
+    [loc[0] + 1, loc[1] + 1]
+  ];
+  var sum = 0;
+  for (var k = 0; k < locs.length; k++) {
+    sum += squares[hash(locs[k])] || 0;
+  }
+  return sum;
+};
+
+const add = function(loc1, loc2) {
+  return [loc1[0] + loc2[0], loc1[1] + loc2[1]];
+};
+
+// Solution 2: use https://oeis.org/A141481
+const solution2 = function(input) {
+  const directions = [[1, 0], [0, 1], [-1, 0], [0, -1]];
+  var squares = {};
+  var loc = [0, 0];
+  var distance = 1;
+  var direction_counter = 2;
+  var direction_index = 0;
+  var distance_counter = distance;
+  squares[hash(loc)] = 1;
+  while (sumOfAdjacentSquares(squares, loc) <= input) {
+    loc = add(loc, directions[direction_index]);
+    squares[hash(loc)] = sumOfAdjacentSquares(squares, loc);
+    distance_counter--;
+    if (distance_counter === 0) {
+      direction_index = (direction_index + 1) % 4;
+      direction_counter--;
+      if (direction_counter === 0) {
+        direction_counter = 2;
+        distance++;
+      }
+      distance_counter = distance;
+    }
+  }
+  return sumOfAdjacentSquares(squares, loc);
+};
+
 const advent03 = function() {
   console.log('Day 3:');
   console.log(solution1(input));
+  console.log(solution2(input));
 };
 
+advent03();
+
 module.exports = advent03;
-// Solution 2: use https://oeis.org/A141481
