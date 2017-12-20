09b241771ebbaa2126e655f75e2d47521033fad1
Douglas Lowder <dlowder@salesforce.com>
2017-12-20 01:29:07 -0800

    Day 20 part 1

--- advent20.js
+++ advent20.js
@@ -13,8 +13,30 @@ const solution2 = function(input) {
 };
 
 const advent20 = function(callback) {
-  io.readInputAsString('./input20.txt', function(input) {
-    let output = 'Day 20: ' + solution1(input) + ' ' + solution2(input);
+  io.readInputAsLines('./input20.txt', function(input) {
+    var re_p = /p=<(.*),(.*),(.*)>/;
+    var re_v = /v=<(.*),(.*),(.*)>/;
+    var re_a = /a=<(.*),(.*),(.*)>/;
+    var p = [];
+    var v = [];
+    var a = [];
+    for (var i = 0; i < input.length; i++) {
+      if ((match = re_p.exec(input[i]))) {
+        p.push([parseInt(match[1]), parseInt(match[2]), parseInt(match[3])]);
+      }
+      if ((match = re_v.exec(input[i]))) {
+        v.push([parseInt(match[1]), parseInt(match[2]), parseInt(match[3])]);
+      }
+      if ((match = re_a.exec(input[i]))) {
+        a.push([parseInt(match[1]), parseInt(match[2]), parseInt(match[3])]);
+      }
+    }
+    var index_of_min_acceleration = a
+      .map(acc => acc[0] * acc[0] + acc[1] * acc[1] + acc[2] * acc[2])
+      .reduce((iMax, x, i, arr) => (x < arr[iMax] ? i : iMax), 0);
+
+    let output =
+      'Day 20: ' + index_of_min_acceleration + ' ' + solution2(input);
     callback && callback(output);
   });
 };
