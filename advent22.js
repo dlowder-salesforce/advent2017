a610c500355bc368919e9dc1c72a1beee0a5626a
Douglas Lowder <dlowder@salesforce.com>
2017-12-22 17:09:03 -0800

    Day 22 part 1

--- advent22.js
+++ advent22.js
@@ -12,9 +12,84 @@ const solution2 = function(input) {
   return '';
 };
 
+const initial_infected_nodes = function(input) {
+  var map = {};
+  for (var j = 0; j < input.length; j++) {
+    for (var i = 0; i < input[j].length; i++) {
+      if (input[j][i] === '#') {
+        map[[i, j]] = 1;
+      }
+    }
+  }
+  return map;
+};
+
+const left_turn = function(dir) {
+  switch (dir) {
+    case 'u':
+      return 'l';
+    case 'l':
+      return 'd';
+    case 'd':
+      return 'r';
+    case 'r':
+      return 'u';
+  }
+};
+
+const right_turn = function(dir) {
+  switch (dir) {
+    case 'u':
+      return 'r';
+    case 'l':
+      return 'u';
+    case 'd':
+      return 'l';
+    case 'r':
+      return 'd';
+  }
+};
+
+const next_pos = function(pos, dir) {
+  switch (dir) {
+    case 'd':
+      return [pos[0], pos[1] + 1];
+    case 'u':
+      return [pos[0], pos[1] - 1];
+    case 'l':
+      return [pos[0] - 1, pos[1]];
+    case 'r':
+      return [pos[0] + 1, pos[1]];
+  }
+};
+
+const burst = function(infected, state) {
+  if (infected[state.pos]) {
+    delete infected[state.pos];
+    state.dir = right_turn(state.dir);
+    state.pos = next_pos(state.pos, state.dir);
+  } else {
+    infected[state.pos] = 1;
+    state.ninfections = state.ninfections + 1;
+    state.dir = left_turn(state.dir);
+    state.pos = next_pos(state.pos, state.dir);
+  }
+};
+
 const advent22 = function(callback) {
-  io.readInputAsString('./input22.txt', function(input) {
-    let output = 'Day 22: ' + solution1(input) + ' ' + solution2(input);
+  io.readInputAsLines('./input22.txt', function(input) {
+    //input = ['..#', '#..', '...'];
+    var infected = initial_infected_nodes(input);
+    var state = {
+      pos: [Math.floor(input.length / 2), Math.floor(input.length / 2)],
+      dir: 'u',
+      ninfections: 0,
+    };
+    for (var i = 0; i < 10000; i++) {
+      burst(infected, state);
+    }
+
+    let output = 'Day 22: ' + state.ninfections + ' ' + solution2(input);
     callback && callback(output);
   });
 };
