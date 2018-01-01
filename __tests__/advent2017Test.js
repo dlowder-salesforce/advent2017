81c5d85155917bff421f9933211c529f66547dff
Douglas Lowder <dlowder@salesforce.com>
2018-01-01 14:32:45 -0800

    test code

--- __tests__/advent2017Test.js
+++ __tests__/advent2017Test.js
@@ -33,86 +33,37 @@ let expected_results = [
   'Day 25: 3145 '
 ];
 
-let check_results = function(i, output, done) {
-  //console.log(output);
-  expect(output).toBe(expected_results[i]);
-  done();
+let run_test = function(i, done) {
+  days[i - 1](function(output) {
+    expect(output).toBe(expected_results[i - 1]);
+    done();
+  });
 };
 
 describe('advent tests', () => {
-  test('Day 1', done => {
-    days[0](output => check_results(0, output, done));
-  });
-  test('Day 2', done => {
-    days[1](output => check_results(1, output, done));
-  });
-  test('Day 3', done => {
-    days[2](output => check_results(2, output, done));
-  });
-  test('Day 4', done => {
-    days[3](output => check_results(3, output, done));
-  });
-  test('Day 5', done => {
-    days[4](output => check_results(4, output, done));
-  });
-  test('Day 6', done => {
-    days[5](output => check_results(5, output, done));
-  });
-  test('Day 7', done => {
-    days[6](output => check_results(6, output, done));
-  });
-  test('Day 8', done => {
-    days[7](output => check_results(7, output, done));
-  });
-  test('Day 9', done => {
-    days[8](output => check_results(8, output, done));
-  });
-  test('Day 10', done => {
-    days[9](output => check_results(9, output, done));
-  });
-  test('Day 11', done => {
-    days[10](output => check_results(10, output, done));
-  });
-  test('Day 12', done => {
-    days[11](output => check_results(11, output, done));
-  });
-  test('Day 13', done => {
-    days[12](output => check_results(12, output, done));
-  });
-  test('Day 14', done => {
-    days[13](output => check_results(13, output, done));
-  });
-  test('Day 15', done => {
-    days[14](output => check_results(14, output, done));
-  });
-  test('Day 16', done => {
-    days[15](output => check_results(15, output, done));
-  });
-  test('Day 17', done => {
-    days[16](output => check_results(16, output, done));
-  });
-  test('Day 18', done => {
-    days[17](output => check_results(17, output, done));
-  });
-  test('Day 19', done => {
-    days[18](output => check_results(18, output, done));
-  });
-  test('Day 20', done => {
-    days[19](output => check_results(19, output, done));
-  });
-  test('Day 21', done => {
-    days[20](output => check_results(20, output, done));
-  });
-  test('Day 22', done => {
-    days[21](output => check_results(21, output, done));
-  });
-  test('Day 23', done => {
-    days[22](output => check_results(22, output, done));
-  });
-  test('Day 24', done => {
-    days[23](output => check_results(23, output, done));
-  });
-  test('Day 25', done => {
-    days[24](output => check_results(24, output, done));
-  });
+  test('Day 1', done => run_test(1, done));
+  test('Day 2', done => run_test(2, done));
+  test('Day 3', done => run_test(3, done));
+  test('Day 4', done => run_test(4, done));
+  test('Day 5', done => run_test(5, done));
+  test('Day 6', done => run_test(6, done));
+  test('Day 7', done => run_test(7, done));
+  test('Day 8', done => run_test(8, done));
+  test('Day 9', done => run_test(9, done));
+  test('Day 10', done => run_test(10, done));
+  test('Day 11', done => run_test(11, done));
+  test('Day 12', done => run_test(12, done));
+  test('Day 13', done => run_test(13, done));
+  test('Day 14', done => run_test(14, done));
+  test('Day 15', done => run_test(15, done));
+  test('Day 16', done => run_test(16, done));
+  test('Day 17', done => run_test(17, done));
+  test('Day 18', done => run_test(18, done));
+  test('Day 19', done => run_test(19, done));
+  test('Day 20', done => run_test(20, done));
+  test('Day 21', done => run_test(21, done));
+  test('Day 22', done => run_test(22, done));
+  test('Day 23', done => run_test(23, done));
+  test('Day 24', done => run_test(24, done));
+  test('Day 25', done => run_test(25, done));
 });
